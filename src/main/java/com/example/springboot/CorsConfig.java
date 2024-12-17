e49f76058c4bc4105c828369ce22202e961872a7
Tarek Najem <tarek.najem@live.com>
2024-12-17 11:34:02 +0100

    modif

--- src/main/java/com/example/springboot/CorsConfig.java
+++ src/main/java/com/example/springboot/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${app.url}")
+    private String allowedOrigin;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins(allowedOrigin)
 			.allowedMethods("*")
 			.allowedHeaders("*")
 			.allowCredentials(true);
